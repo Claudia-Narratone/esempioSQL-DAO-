021a120cf00d855d3cbc8041c32c0db36c2aeeb9
cnarr <cnarr@LAPTOP-L1JET7EF.wind3.hub>
2020-03-27 18:49:04 +0100

    esempio pattern DAO finito

---
+++ src/main/java/it/polito/tdp/esempioSQL/db/BabsDAO.java
@@ -0,0 +1,73 @@
+package it.polito.tdp.esempioSQL.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Station;
+
+public class BabsDAO {
+
+	public List<Station> listStation(){
+		List<Station> result=new ArrayList<Station>();
+		
+		String sql="SELECT station_id, name, dockcount, landmark FROM STATION";
+		
+		try {
+			Connection conn=DBConnect.getConnection();
+			
+
+			
+			PreparedStatement st=conn.prepareStatement(sql);
+			
+			ResultSet res=st.executeQuery(sql);
+			
+			while(res.next()) {
+				Station s=new Station(res.getInt("station_id"), res.getString("name"), res.getInt("dockcount"), res.getString("landmark"));
+				result.add(s);
+			}
+			st.close();
+			
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	
+	public List<Station> listStationByLandmark(String landmark){
+List<Station> result=new ArrayList<Station>();
+		
+		String sql="SELECT station_id, name, dockcount, landmark FROM STATION WHERE landmark=? ORDER BY name";
+		
+		
+		try {
+			Connection conn=DBConnect.getConnection();
+			
+			PreparedStatement st=conn.prepareStatement(sql);
+			
+			st.setString(1, landmark);
+			ResultSet res=st.executeQuery(sql);
+			
+			while(res.next()) {
+				Station s=new Station(res.getInt("station_id"), res.getString("name"), res.getInt("dockcount"), res.getString("landmark"));
+				result.add(s);
+			}
+			st.close();
+			
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
